a986ec08edc55cad10b57842be9be13a6e0fc292
CkTech123 <132186774+CkTech123@users.noreply.github.com>
2023-05-11 13:14:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,156 +1,105 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 3000;
+const bcrypt = require("bcryptjs");
 const { MongoClient } = require("mongodb");
-// const url = "mongodb://localhost:27017";
-const url = "mongodb+srv://query:Query@query.thd3wks.mongodb.net/test"
-// const database = "querybox";
-const database = "querybox"
+// const url = "mongodb://localhost:27017"
+    const url = "mongodb+srv://CAASS:CAASScaass@caass.irtykc7.mongodb.net/test";
 const client = new MongoClient(url);
+const database = "caass";
+const table = "register_login";
 const bodyParser = require("body-parser");
-const cors = require('cors')
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(cors())
-    // app.get('/chat',(req,res)=>{
-    //     res.sendFile(__dirname)
-    // })
-app.post("/msg", (req, res) => {
+app.use(cors());
+app.post("/register", async(req, res) => {
+    //Register api, first of all finding the email if alredady registered or not,
+    //then hashing the password and storing intt mongodb
     try {
+        const db = await dbConnect(table);
         const name = req.body.name;
-        const d = new Date()
-        const date = d.toDateString()
-        const time = d.toTimeString()
-        const msg = req.body.msg;
-        const table = "messages"
-        console.log(req.body)
-        console.log(date + time)
-        const result = push(table, name, date, time, msg)
-            // console.log(result)
-        result.then(function(val) {
-            console.log(val)
-            if (val) {
-
-                res.send("Query Sent, Wait for Response..!<br/>Go back and refresh the page to view your query")
-
+        const email = req.body.email;
+        const password = req.body.password;
+        let find = await db.findOne({ email: email });
+        if (find != undefined) {
+            return {
+                statusCode: 500,
+                body: "Email Already Registered",
+            };
+        } else {
+            const hash = bcrypt.hashSync(password, 8);
+            const result = await db.insertOne({
+                name: name,
+                email: email,
+                password: hash,
+            });
+            // console.log(result.acknowledged)
+            if (result.acknowledged) {
+                return {
+                    statusbar: 200,
+                    body: "User Registered",
+                };
             } else {
-                res.send("Error pushing data")
+                return {
+                    statusCode: 500,
+                    body: "Error in register",
+                };
             }
-        })
+        }
     } catch (e) {
         return {
             statusCode: 500,
-            body: e.message
+            body: e.message,
         };
     }
 });
-app.get('/msg', (req, res) => {
-    const table = "messages"
-    const data = show(table)
-    data.then(function(val) {
-        // console.log(val)
-        res.json(val)
-    })
-
-})
-app.get('/count', (req, res) => {
-    const table = "messages"
-    const data = show(table)
-    var c = 0;
-    data.then(function(val) {
-        val.forEach(function(value, index) {
-            c = index + 1;
-        })
-        console.log(c)
-        res.status(200).send(String(c));
-    });
-
-
-})
-app.post('/review', async(req, res) => {
+app.post("/login", async(req, res) => {
     try {
-        const table = "reviews";
-        const name = req.body.name;
-        const message = req.body.message;
-        const db = await dbConnect(table);
-        // console.log(db)
-        // console.log(req.body)
-        const result = await db.insertOne({
-                name: name,
-                message: message
-            })
-            // console.log(result)
-        if (result.acknowledged) {
-            // res.status(200).send("Review Saved")
-            return {
-                statusCode: 200,
-                body: "Review Saved"
-            };
-        } else {
-            // res.status(500).send("Something went wrong!, Try again")
-            return {
-                statusCode: 500,
-                body: "Something went wrong!, Try again"
-            };
-        }
-    } catch (e) {
-        return e.message;
-    }
-})
-app.get('/getReviews', async(req, res) => {
-    try {
-        const table = "reviews"
-        const db = await dbConnect(table);
-        const result = await db.find({}).toArray();
-        // console.log(result)
-        const arrLength = result.length - 1;
-        if (result != undefined) {
-            const Response = {
-                "review1": result[arrLength],
-                "review2": result[arrLength - 1],
-                "review3": result[arrLength - 2]
+        const email = req.body.email;
+        const password = req.body.password;
+        let db = await dbConnect(table);
+        const info = await db.findOne({ email: email });
+        // console.log(info)
+        if (info != undefined) {
+            if (bcrypt.compareSync(password, info.password)) {
+                return {
+                    statusCode: 200,
+                    body: "Login Success",
+                };
+            } else {
+                return {
+                    statusCode: 500,
+                    body: "Password doesn't match",
+                };
             }
-            console.log(Response)
-            res.status(200).send(Response);
         } else {
-            res.status(404).send("No Review Found")
+            return {
+                statusCode: 404,
+                body: "Email Not Registered",
+            };
         }
     } catch (e) {
-        res.status(500).send(e.message)
+        return {
+            statusCode: 500,
+            body: e.message,
+        };
     }
-})
-app.listen(port, () => {
-    console.log("Server started at port " + port);
 });
+
 async function dbConnect(table) {
     try {
         const result = await client.connect();
-        const db = result.db(database);
+        let db = result.db(database);
         return db.collection(table);
     } catch (e) {
-        return e.message;
+        return {
+            statusCode: 500,
+            body: e.message,
+        };
     }
 }
-const push = async(table, name, date, time, msg) => {
-    try {
-        const db = await dbConnect(table);
-        const result = await db.insertOne({
-            name: name,
-            date: date,
-            time: time,
-            msg: msg,
-        });
-        return result.acknowledged
-    } catch (e) {
-        return e.message
-    }
-};
-const show = async(table) => {
-    try {
-        const db = await dbConnect(table)
-        const data = db.find({}).toArray();
-        return data
-    } catch (e) {
-        return e.message
-    }
-}
+
+app.listen(port, () => {
+    console.log("Server started at port " + port);
+});
